c42e4a1270dfa16f3f81024d3d3a0bf40d4de607
Matheus <araujomatheusventura@gmail.com>
2022-11-29 17:13:07 -0300

    disponiblização do arquivo para inserção de dados no banco

---
+++ Dados_Banco_mwemp.sql
@@ -0,0 +1,29 @@
+
+INSERT INTO  `mwemp`.`ufs` (`id`, `sigla`, `nome`, `created_at`, `updated_at`) VALUES
+( 1 , 'AL' , 'Alagoas', NULL, NULL),
+( 2 , 'BA' , 'Bahia', NULL, NULL),
+( 3 , 'SE' , 'Sergipe', NULL, NULL);
+
+
+INSERT INTO  `mwemp`.`municipios` (`id`, `nome`, `id_uf` , `created_at`, `updated_at`) VALUES
+( 1 , ' Água Branca ' , 1, NULL, NULL ),
+( 2 , ' Anadia ' , 1, NULL, NULL ),
+( 3 , ' Arapiraca ' , 1, NULL, NULL ),
+( 4 , ' Barra de Santo Antônio ' , 1, NULL, NULL ),
+( 5 , ' Barra de São Miguel ' , 1, NULL, NULL ),
+( 6 , ' Belém ' , 1, NULL, NULL ),
+( 7 , ' Belo Monte ' , 1, NULL, NULL ),
+( 11 , ' Paulo Afonso ' , 2, NULL, NULL ),
+( 12 , ' Abaré ' , 2, NULL, NULL ),
+( 13 , ' Acajutiba ' , 2, NULL, NULL ),
+( 14 , ' Adustina ' , 2, NULL, NULL ),
+( 15 , ' Aiquara ' , 2, NULL, NULL ),
+( 16 , ' Alagoinhas ' , 2, NULL, NULL ),
+( 17 , ' Almadina ' , 2, NULL, NULL ),
+(18, 'Aracaju' , 3, NULL, NULL ),
+(19, 'Areia Branca' , 3, NULL, NULL ),
+(20, 'Barra dos Coqueiros' , 3, NULL, NULL ),
+(21, 'Canhoba' , 3, NULL, NULL ),
+(22, 'Canindé de São Francisco' , 3, NULL, NULL ),
+(23, 'Carira' , 3, NULL, NULL ),
+(24, 'Carmópolis' , 3, NULL, NULL );
