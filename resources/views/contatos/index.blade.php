f02680e53d8f4e10163d89a052ad9540b8a3bcb5
unknown <descartusavelmente@gmail.com>
2022-12-05 13:48:46 -0300

    organização de contatos e produtos

--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -19,19 +19,20 @@
 
         <!--<div class="text-white rounded-3 shadow-lg p-1 m-2" style="background-color: #2D2D3C; text-align: center;">-->
         <!--</div>-->
+        <div class="container border border-2 pt-1 pb-1 p-5 rounded-4 mb-4 shadow" style="background-color: #2D2D3C;">
+            <h2 class="fs-1 text-center text-light">Fale conosco</h2>
+        </div>
 
-        <h2 class="mb-4 fs-1 fs-1" style="margin-top: -60px">Fale conosco</h2>
+        <div class="text-white rounded-4 shadow-lg p-1 pb-2 mb-2 px-4" style="background-color: #2D2D3C;">
 
-        <div class="text-white rounded-4 shadow-lg p-1 pb-2 m-2 px-4" style="background-color: #2D2D3C;">
+            <form class="row p-3">
 
-            <form class="row g-2 p-3">
-
-                <div class="form-group col-6">
+                <div class="form-group mb-3 col-6">
                     <label for="nome">Nome</label>
                     <input type="text" class="form-control" name="nome" id="nome" required>
                 </div>
 
-                <div class="form-group col-6">
+                <div class="form-group mb-3 col-6">
                     <label for="nome">Estado</label>
                     <select class="form-select" id="estado" name="estado" required>
                         <option value=""></option>
@@ -40,12 +41,12 @@
                     </select>
                 </div>
 
-                <div class="form-group col-6">
+                <div class="form-group mb-3 col-6">
                     <label for="email">Email</label>
                     <input name="email" id="email" type="email" class="form-control" required>
                 </div>
 
-                <div class="form-group col-6">
+                <div class="form-group mb-3 col-6">
                     <label for="assunto">Assunto</label>
                     <select class="form-select" id="assunto" name="assunto" required>
                         <option value=""></option>
@@ -53,32 +54,46 @@
                         <option value=""></option>
                     </select>
                 </div>
-
-                <div class="form-group col-6">
-                    <label for="telefone">Telefone</label>
-                    <input type="text" class="form-control" placeholder="(DDD)XXXXX-XXXX" name="telefone" id="telefone"
-                        required>
+                <div class="col-6">
+                    <div class="form-group mb-3">
+                        <label for="telefone">Telefone</label>
+                        <input type="text" class="form-control" placeholder="(DDD)XXXXX-XXXX" name="telefone" id="telefone"
+                            required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="cidade">Cidade</label>
+                        <select class="form-select" id="cidade" name="cidade" required>
+                            <option value=""></option>
+                            <option value=""></option>
+                            <option value=""></option>
+                        </select>
+                    </div>
                 </div>
 
-                <div class="form-group col-6">
+                <div class="form-group mb-3 col-6">
                     <label for="mensagem">Mensagem</label>
                     <input type="text" class="form-control pb-5" name="mensagem" id="mensagem" required>
                 </div>
 
-                <div class="form-group col-6">
-                    <label for="cidade">Cidade</label>
-                    <select class="form-select" id="cidade" name="cidade" required>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                    </select>
+                
+            <hr style="color: white;">
+
+            <div class="mb-2 mt-2 container text-center">
+                <div class="row">
+                  <div class="col text-end">
+                    <a href="#" class="ps-4 pe-4 btn btn-danger btn-lg">Cancelar</a>
+                  </div>
+                  <div class="col text-start">
+                    <a href="#" class=" btn btn-info btn-lg text-light">Confirmar</a>
+                  </div>
                 </div>
-            </form>
-            <div class="pb-4">
-                <a href="#" class="btn btn-danger btn-lg col-md-2" style="margin-left: 400px;">Cancelar</a>
-                <button type="submit" class="btn btn-lg col-md-2"
-                    style="margin-left: 20px; background-color: #5EB7CB">Confirmar</button>
             </div>
+            </form>
+            {{-- <div class="align-middle pb-4 row">
+                <a href="#" class=" aling-end btn btn-danger btn-lg" style="max-width: 20px">Cancelar</a>
+                <button type="submit" class="align-start btn btn-lg col-md-6"
+                    style="background-color: #5EB7CB">Confirmar</button>
+            </div> --}}
         </div>
     </div>
 @endsection
--- resources/views/servicos/index.blade.php
+++ resources/views/servicos/index.blade.php
@@ -4,62 +4,116 @@
 
 @section('conteudo')
 
-<div style="position:relative;">
-    <img class="w-100" style="height: 370px; margin-top: -50px; object-fit: cover; filter: brightness(55%);"
-        src="/images/banner_produtos.png">
-</div>
+    <div style="position:relative;">
+        <img class="w-100" style="height: 370px; margin-top: -50px; object-fit: cover; filter: brightness(55%);"
+            src="/images/banner_produtos.png">
+    </div>
 
-<h2 class="m-5">Conheça nossos serviços</h2>
+    <div class="container border border-2 mt-4 pt-1 pb-1 p-5 rounded-4 mb-4 shadow" style="background-color: #2D2D3C;">
+        <h2 class="fs-1 text-center text-light">Conheça nossos serviços</h2>
+    </div>
 
-<div class="row m-5 mb-5 container-fluid" style="">
+    <div class="container">
+        <div class="row">
 
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/banheiro_quimico.jpg">
-</div>
+            <div class="col-4 text-center p-2 d-flex align-items-stretch">
+                <div class="rounded-4 bg-dark p-3" style="max-height: 500px;">
+                    <div class="align-middle" style="max-height: 200px;">
+                        <img src="/images/banheiro_quimico.jpg" style="max-height: 200px" alt="banheiro">
+                    </div>
+                    <p class="text-light text-break">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="col-4 text-center p-2 d-flex align-items-stretch">
+                <div class="rounded-4 bg-dark p-3" style="max-height: 500px;">
+                    <div class="align-middle" style="max-height: 200px;">
+                        <img src="/images/camarim.jpg" style="max-height: 200px" alt="banheiro">
+                    </div>
+                    <p class="text-light text-break">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="col-4 text-center p-2 d-flex align-items-stretch">
+                <div class="rounded-4 bg-dark p-3" style="max-height: 500px;">
+                    <div class="align-middle" style="max-height: 200px;">
+                        <img src="/images/diciplinadores.jpg" style="max-height: 200px" alt="banheiro">
+                    </div>
+                    <p class="text-light text-break">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="col-4 text-center p-2 d-flex align-items-stretch">
+                <div class="rounded-4 bg-dark p-3" style="max-height: 500px;">
+                    <div class="align-middle" style="max-height: 200px;">
+                        <img src="/images/foto_aluminio_p30.jpg" style="max-height: 200px" alt="banheiro">
+                    </div>
+                    <p class="text-light text-break">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="col-4 text-center p-2 d-flex align-items-stretch">
+                <div class="rounded-4 bg-dark p-3" style="max-height: 500px;">
+                    <div class="align-middle" style="max-height: 200px;">
+                        <img src="/images/diciplinadores.jpg" style="max-height: 200px" alt="banheiro">
+                    </div>
+                    <p class="text-light text-break">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="col-4 text-center p-2 d-flex align-items-stretch">
+                <div class="rounded-4 bg-dark p-3" style="max-height: 500px;">
+                    <div class="align-middle" style="max-height: 200px;">
+                        <img src="/images/diciplinadores.jpg" style="max-height: 200px" alt="banheiro">
+                    </div>
+                    <p class="text-light text-break">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            
+           
+            
+            {{--<div class="col-sm-4" style="">
+                <img class="" src="/images/banheiro_quimico.jpg">
+            </div>
 
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/camarim.jpg">
-</div>
+            <div class="col-4" style="">
+                <img style="height: 250px; width: 250px;" src="/images/camarim.jpg">
+            </div>
 
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/diciplinadores.jpg">
-</div>
+            <div class="col-4" style="">
+                <img style="height: 250px; width: 250px;" src="/images/diciplinadores.jpg">
+            </div>
 
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/foto_aluminio_p30.jpg">
-</div>
+            <div class="col-4" style="">
+                <img style="height: 250px; width: 250px;" src="/images/foto_aluminio_p30.jpg">
+            </div>
 
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/foto_palco_14x10.jpg">
-</div>
+            <div class="col-4" style="">
+                <img style="height: 250px; width: 250px;" src="/images/foto_palco_14x10.jpg">
+            </div>
 
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/foto_palco_14x12.jpg">
-</div>
+            <div class="col-4" style="">
+                <img style="height: 250px; width: 250px;" src="/images/foto_palco_14x12.jpg">
+            </div>
 
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/gerador.jpg">
-</div>
+            <div class="col-4" style="">
+                <img style="height: 250px; width: 250px;" src="/images/gerador.jpg">
+            </div>
 
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/palco_praticavel.png">
-</div>
+            <div class="col-4" style="">
+                <img style="height: 250px; width: 250px;" src="/images/palco_praticavel.png">
+            </div>
 
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/placa_fechamento.png">
-</div>
+            <div class="col-4" style="">
+                <img style="height: 250px; width: 250px;" src="/images/placa_fechamento.png">
+            </div>
 
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/tenda_5x5.jpg">
-</div>
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/tenda_6x6.jpg">
-</div>
+            <div class="col-4" style="">
+                <img style="height: 250px; width: 250px;" src="/images/tenda_5x5.jpg">
+            </div>
+            <div class="col-4" style="">
+                <img style="height: 250px; width: 250px;" src="/images/tenda_6x6.jpg">
+            </div>
 
-<div class="col-4" style="">
-    <img style="height: 250px; width: 250px;" src="/images/toldo_4x4.jpeg">
-</div>
-
-</div>
+            <div class="col-4" style="">
+                <img style="height: 250px; width: 250px;" src="/images/toldo_4x4.jpeg">
+            </div> --}}
+        </div>
+    </div>
         
 @endsection
